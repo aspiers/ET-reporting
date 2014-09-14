316fa366cd65349268b5556d98400e8fb9955af0
Adam Spiers <hack4good@adamspiers.org>
2014-09-14 11:24:47 +0100

    reorder routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  mount Surveyor::Engine => "/surveys", :as => "surveyor"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'surveys#index'
+
+  mount Surveyor::Engine => "/surveys", :as => "surveyor"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
